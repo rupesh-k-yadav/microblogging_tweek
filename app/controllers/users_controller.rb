c6cfe4d889134b61f330a790bcaf3e37e3b03332
rupesh_shipx <rupesh@shipx.in>
2022-03-31 15:00:08 +0530

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
     #redirect_to root_url and return unless FILL_IN
   end
   
@@ -49,21 +50,23 @@ class UsersController < ApplicationController
     flash[:success] = "User deleted"
     redirect_to users_url
   end
+  
   private
     def user_params
       params.require(:user).permit(:name, :email, :password,
-                                      :password_confirmation)
+                                   :password_confirmation)
     end
     
     # Before filters
+
     # Confirms a logged-in user.
-    def logged_in_user
-      unless logged_in?
-          store_location
-          flash[:danger] = "Please log in."
-          redirect_to login_url
-       end
-    end
+    #def logged_in_user
+    #  unless logged_in?
+    #      store_location
+    #     flash[:danger] = "Please log in."
+    #     redirect_to login_url
+    #   end
+    #end
 
     # Confirms the correct user.
     def correct_user
